f534468ee3ec318fdefd34b5b39627a14584c685
zeoio <kinsleer@outlook.com>
2019-04-24 21:46:51 +0800

    删除版权声明的信息

--- addrmgr/doc.go
+++ addrmgr/doc.go
@@ -1,8 +1,3 @@
-// Copyright (c) 2014 The btcsuite developers
-// Copyright (c) 2015-2016 The Decred developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
 /*
 Package addrmgr implements concurrency safe Decred address manager.
 
--- bech32/bech32.go
+++ bech32/bech32.go
@@ -1,8 +1,3 @@
-// Copyright (c) 2017 The btcsuite developers
-// Copyright (c) 2019 The Decred developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
 package bech32
 
 import (
