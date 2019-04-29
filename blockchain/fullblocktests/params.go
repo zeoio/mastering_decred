f77fe723e75f44fc9a61265015ebd8ac789c0626
zeoio <kinsleer@outlook.com>
2019-04-29 20:35:57 +0800

    删除版权信息

--- blockchain/fullblocktests/params.go
+++ blockchain/fullblocktests/params.go
@@ -1,7 +1,3 @@
-// Copyright (c) 2016-2019 The Decred developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
 package fullblocktests
 
 import (
