4db43bbd9e586b7de2cd968517b67eb193e2aa90
zeoio <kinsleer@outlook.com>
2019-05-05 20:55:11 +0800

    删除版权信息

--- dcrjson/chainsvrcmds.go
+++ dcrjson/chainsvrcmds.go
@@ -1,8 +1,3 @@
-// Copyright (c) 2014 The btcsuite developers
-// Copyright (c) 2015-2019 The Decred developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
 // NOTE: This file is intended to house the RPC commands that are supported by
 // a chain server.
 
